0df3a550db99f7586fddda9cbaf9fd8d74f1b959
Sebastian Martinez <Luismartinez24210@gmail.com>
2026-04-14 18:49:58 -0500

    chore : create project folder structure

---
+++ src/test/java/com/SebasMa24/PruebaTecnica/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SebasMa24.PruebaTecnica.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
