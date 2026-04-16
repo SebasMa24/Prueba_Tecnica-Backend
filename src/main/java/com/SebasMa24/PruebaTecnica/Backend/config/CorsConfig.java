921696a258b61332dca5f3ad75d414c65d60acae
Sebastian Martinez <Luismartinez24210@gmail.com>
2026-04-16 13:23:32 -0500

    Fix (Cors)

--- src/main/java/com/SebasMa24/PruebaTecnica/Backend/config/CorsConfig.java
+++ src/main/java/com/SebasMa24/PruebaTecnica/Backend/config/CorsConfig.java
@@ -3,9 +3,10 @@ package com.SebasMa24.PruebaTecnica.Backend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer{
     @Value("${app.cors.allowed-origins}")
     private String allowedOrigins;
 
